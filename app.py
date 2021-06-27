17bab5126d957c719dbbf419e7b21d59e9d666d8
YiFei Li <yifeil@berkeley.edu>
2021-06-27 11:15:44 -0700

    refactor: initialize new version of api, prepare for login implementation

--- app.py
+++ app.py
@@ -8,18 +8,19 @@ from src.department import Department, DepartmentList
 from src.seat import Seat, SeatList
 from src.contact import ContactForm
 from flask import Flask, abort, request, jsonify, render_template
+import flask_restful
 from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
-api.add_resource(Course, '/course')
-api.add_resource(CourseList, '/course_list')
 api.add_resource(Department, '/department')
 api.add_resource(DepartmentList, '/department_list')
-api.add_resource(Seat, '/seat')
+api.add_resource(Course, '/course')
+api.add_resource(CourseList, '/course_list')
 api.add_resource(SeatList, '/seat_list')
+api.add_resource(Seat, '/seat')
 api.add_resource(ContactForm, '/contact')
 
 @app.route('/')
@@ -27,4 +28,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, ssl_context='adhoc')
+    app.run(host="127.0.0.1", port=5000, debug=True, ssl_context='adhoc')
