40288d9d84bf2bed724f8832a1c7fefaa7c3142c
yzhu <yzhu@ucla.edu>
2021-06-28 00:04:11 -0700

    fix: typo in login_manager.py

--- app.py
+++ app.py
@@ -18,38 +18,39 @@ CORS(app)
 
 # Intialize API Resource
 api = Api(app)
-api.add_resource(ContactForm, '/contact')
-api.add_resource(Course, '/course')
-api.add_resource(CourseList, '/course_list')
-api.add_resource(Department, '/department')
-api.add_resource(DepartmentList, '/department_list')
-api.add_resource(SeatList, '/seat_list')
-api.add_resource(Seat, '/seat')
+api.add_resource(ContactForm, "/contact")
+api.add_resource(Course, "/course")
+api.add_resource(CourseList, "/course_list")
+api.add_resource(Department, "/department")
+api.add_resource(DepartmentList, "/department_list")
+api.add_resource(SeatList, "/seat_list")
+api.add_resource(Seat, "/seat")
 
 # Setup Login Manager
 okta_helper = OktaHelper()
-from src.auth.login_manager import access_token_required, \
-    id_token_required
+from src.auth.login_manager import access_token_required, id_token_required
 
 
 # Some examples of using okta authorization login manager.
 # Example 1: no login is needed.
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Example 2: require valid access token.
-@app.route('/example_access_token_check')
+@app.route("/example_access_token_check")
 @access_token_required
 def example_access_token_check(uid):
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Example 3: require valid ID token.
-@app.route('/example_id_token_check')
+@app.route("/example_id_token_check")
 @id_token_required
 def example_id_token_check(uid):
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context='adhoc'
-            )
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True, ssl_context="adhoc")
--- src/auth/login_manager.py
+++ src/auth/login_manager.py
@@ -4,38 +4,45 @@ from functools import wraps
 
 
 def access_token_required(func):
-    """ Decorator to check whether the request has valid access token.
+    """Decorator to check whether the request has valid access token.
 
     Wraps a function and check whether the incoming request has a vlid
-    access token. If the access token is invalid, return a 403 
-    unauthorized error as response. Otherwise, pass to the original 
+    access token. If the access token is invalid, return a 403
+    unauthorized error as response. Otherwise, pass to the original
     function with user id.
     """
+
     @wraps(func)
     def decorated_view(*args, **kwargs):
-        (is_authorized, error_message_or_uid) = \
-            okta_helper.is_access_request_authorized(request)
+        (
+            is_authorized,
+            error_message_or_uid,
+        ) = okta_helper.is_access_request_authorized(request)
         if is_authorized:
             return func(error_message_or_uid)
         else:
-            return make_response(error_message, 403)
+            return make_response(error_message_or_uid, 403)
+
     return decorated_view
 
 
 def id_token_required(func):
-    """ Decorator to check whether the request has valid ID token.
+    """Decorator to check whether the request has valid ID token.
 
     Wraps a function and check whether the incoming request has a vlid
     ID token. If the ID token is invalid, return a 403 unauthorized
     error as response. Otherwise, pass to the original function with
     user id.
     """
+
     @wraps(func)
     def decorated_view():
-        (is_authorized, error_message_or_uid) = \
-            okta_helper.is_id_request_authorized(request)
+        (is_authorized, error_message_or_uid) = okta_helper.is_id_request_authorized(
+            request
+        )
         if is_authorized:
             return func(error_message_or_uid)
         else:
             return make_response(error_message_or_uid, 403)
+
     return decorated_view
